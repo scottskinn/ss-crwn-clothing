102a2559825979361c9725011c4b66b59ceb3954
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-06-23 16:04:39 -0400

    home page links

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,10 +1,19 @@
-import { BackGroundImg, Body, DirectoryContainer } from './directory-item.style.jsx';
+import { useNavigate } from 'react-router-dom';
+
+import { 
+  BackGroundImg, 
+  Body, 
+  DirectoryContainer 
+} from './directory-item.style.jsx';
 
 const DirectoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageUrl, title, route } = category;
+    const navigate = useNavigate();
+
+    const navigateHandler = () => navigate(route);
     
     return (
-        <DirectoryContainer>
+      <DirectoryContainer onClick={navigateHandler}>
         <BackGroundImg imageUrl={imageUrl} />
         <Body>
           <h2>{title}</h2>
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,12 +2,44 @@ import { CategoriesContainer } from './directory.style';
 
 import DirectoryItem from '../directory-item/directory-item.component';
 
-const CategoryMenu = ({ categories }) => {
+const categories = [
+  {
+    id: 1,
+    title: 'hats',
+    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    route: 'shop/hats',
+  },
+  {
+    id: 2,
+    title: 'jackets',
+    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    route: 'shop/jackets',
+  },
+  {
+    id: 3,
+    title: 'sneakers',
+    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    route: 'shop/sneakers',
+  },
+  {
+    id: 4,
+    title: 'womens',
+    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    route: 'shop/womens',
+  },
+  {
+    id: 5,
+    title: 'mens',
+    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    route: 'shop/mens',
+  },
+];
+
+const CategoryMenu = () => {
     return (
         <CategoriesContainer>
-          {categories.map((category) => (
-          
-          <DirectoryItem key={category.id} category={category} />
+          { categories.map((category) => (
+            <DirectoryItem key={category.id} category={category} />
           ))}
         </CategoriesContainer>
     );

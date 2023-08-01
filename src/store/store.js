f730721f1053abb615784ef006011f3f1512c622
scottskinn <84105761+scottskinn@users.noreply.github.com>
2023-08-01 16:18:12 -0400

    update logo

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,10 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+
 import { rootSaga } from './root-saga';
-import createSagaMiddleware from '@redux-saga/core';
 
 import { rootReducer } from './root-reducer';
 
@@ -12,51 +12,31 @@ const persistConfig = {
   key: 'root',
   storage,
   whitelist: ['cart'],
-}
+};
 
 const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
-  process.env.NODE_ENV !== 'production' && logger, 
-  sagaMiddleware
+  process.env.NODE_ENV !== 'production' && logger,
+  sagaMiddleware,
 ].filter(Boolean);
-
-// const thunkMiddleware = (store) => (next) => (action) => {
-//   if(typeof(action) === 'function') {
-//     action(dispatch)
-//   }
-// }
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
-    window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
-  || compose;
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
-const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
   undefined,
-  composeEnhancers
+  composedEnhancers
 );
 
 sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
-
-
-// const loggerMiddleware = (store) => (next) => (action) => {
-//   if (!action.type) {
-//     return next(action);
-//   }
-
-//   console.log('type: ', action.type);
-//   console.log('payload: ', action.payload);
-//   console.log('currentState: ', store.getState());
-
-//   next(action);
-
-//   console.log('next state: ', store.getState());
-// };
